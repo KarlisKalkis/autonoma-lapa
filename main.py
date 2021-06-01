f56c6f154dbc091113b7e8f4df5b70e28e0bf213
DP4KKalkis <>
2021-06-01 10:48:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask('app')
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+
+app.run(host='0.0.0.0', port=8080)
