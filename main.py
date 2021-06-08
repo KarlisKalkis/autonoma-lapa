a234fde07ad55f5cd3240293ffbd81722392aa4c
DP4SmitsMatiss <>
2021-06-08 12:16:10 +0000

    viss

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def index():
 def par():
     return render_template("par.html")
 
-@app.route('/parmums')
-def parmums():
+@app.route('/about')
+def about():
   return render_template("par.html")
     
     
@@ -21,17 +21,27 @@ def parmums():
 @app.route('/klienti')
 def klienti():
     return render_template("klienti.html")
+@app.route('/clients')
+def clients():
+    return render_template("klienti.html")
 
 @app.route('/soferis')
 def soferis():
   return render_template("soferis.html")
+@app.route('/chauffer')
+def chauffer():
+    return render_template("klienti.html")
 
 @app.route('/rezervet')
 def rezervet():
   return render_template("book.html")
+@app.route('/book')
+def book():
+    return render_template("klienti.html")
 
 @app.route('/atgriezt')
 def atgriezt():
   return render_template("atgriezt.html")  
+
     
 app.run(host='0.0.0.0', port=8080)
